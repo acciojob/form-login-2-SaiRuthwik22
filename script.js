39ba9c21930ad68cf617e1b3017d2b6eecdf5ec2
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-07 16:06:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 function handle(event) {
 	event.preventDefault();
 	let firstName = document.getElementById("FirstName").value
-	let LastName = document.getElementById("LastName").value
-	let PhoneNumber = document.getElementById("PhoneNumber").value
-	let EmailID = document.getElementById("EmailID").value
+	let lastName = document.getElementById("LastName").value
+	let phoneNumber = document.getElementById("PhoneNumber").value
+	let emailID = document.getElementById("EmailID").value
 	alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailID}`);
 }
