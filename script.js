d0d58463a22f015eeb5d897c6e5e3f39b03ecb03
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-07 16:03:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //your JS code here. If required.
 function handle(event) {
+	event.preventDefault();
 	let firstName = document.getElementById("FirstName").value
 	let LastName = document.getElementById("LastName").value
 	let PhoneNumber = document.getElementById("PhoneNumber").value
 	let EmailID = document.getElementById("EmailID").value
-	alert(`First Name: ${firstName}\nLast Name: ${lastName}\nPhone Number: ${phoneNumber}\nEmail ID: ${emailID}`);
+	alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailID}`);
 }
