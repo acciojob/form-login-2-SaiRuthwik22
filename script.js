ee263da0b366a34e8ca1ae0bd88ee09e6d91bf72
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-07 15:33:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function handle(event) {
+	let firstName = document.getElementById("FirstName").value
+	let LastName = document.getElementById("LastName").value
+	let PhoneNumber = document.getElementById("PhoneNumber").value
+	let EmailID = document.getElementById("EmailID").value
+	alert(`First Name: ${firstName} Last Name: ${LastName} Phone Number: ${PhoneNumber} Email ID: ${EmailID}`)
+}
